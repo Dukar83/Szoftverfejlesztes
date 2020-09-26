533c26ac27c6ee8ab114619f8a1073af2f108efa
Dukar83 <66202113+Dukar83@users.noreply.github.com>
2020-09-26 13:51:53 +0200

    mozgatos kész

---
+++ Mozgatós/mozgatos/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace mozgatos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnMeretNovel = new System.Windows.Forms.Button();
+            this.btnMeretCsokkent = new System.Windows.Forms.Button();
+            this.btnTop = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnCenter = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnMaxRight = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnMaxLeft = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnBot = new System.Windows.Forms.Button();
+            this.btnAtlathatoNovel = new System.Windows.Forms.Button();
+            this.btnAtlathatoCsokkent = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.btnBot);
+            this.panel1.Controls.Add(this.btnDown);
+            this.panel1.Controls.Add(this.btnMaxLeft);
+            this.panel1.Controls.Add(this.btnLeft);
+            this.panel1.Controls.Add(this.btnMaxRight);
+            this.panel1.Controls.Add(this.btnRight);
+            this.panel1.Controls.Add(this.btnCenter);
+            this.panel1.Controls.Add(this.btnUp);
+            this.panel1.Controls.Add(this.btnTop);
+            this.panel1.Location = new System.Drawing.Point(0, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 600);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnMeretNovel
+            // 
+            this.btnMeretNovel.Location = new System.Drawing.Point(41, 53);
+            this.btnMeretNovel.Name = "btnMeretNovel";
+            this.btnMeretNovel.Size = new System.Drawing.Size(75, 23);
+            this.btnMeretNovel.TabIndex = 0;
+            this.btnMeretNovel.Text = "Növel";
+            this.btnMeretNovel.UseVisualStyleBackColor = true;
+            this.btnMeretNovel.Click += new System.EventHandler(this.btnMeretNovel_Click);
+            // 
+            // btnMeretCsokkent
+            // 
+            this.btnMeretCsokkent.Location = new System.Drawing.Point(144, 53);
+            this.btnMeretCsokkent.Name = "btnMeretCsokkent";
+            this.btnMeretCsokkent.Size = new System.Drawing.Size(75, 23);
+            this.btnMeretCsokkent.TabIndex = 1;
+            this.btnMeretCsokkent.Text = "Csökkent";
+            this.btnMeretCsokkent.UseVisualStyleBackColor = true;
+            this.btnMeretCsokkent.Click += new System.EventHandler(this.btnMeretCsokkent_Click);
+            // 
+            // btnTop
+            // 
+            this.btnTop.Location = new System.Drawing.Point(337, 170);
+            this.btnTop.Name = "btnTop";
+            this.btnTop.Size = new System.Drawing.Size(75, 23);
+            this.btnTop.TabIndex = 2;
+            this.btnTop.Text = "Tetejére";
+            this.btnTop.UseVisualStyleBackColor = true;
+            this.btnTop.Click += new System.EventHandler(this.btnTop_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(337, 222);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(75, 23);
+            this.btnUp.TabIndex = 3;
+            this.btnUp.Text = "Fel";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnCenter
+            // 
+            this.btnCenter.Location = new System.Drawing.Point(337, 276);
+            this.btnCenter.Name = "btnCenter";
+            this.btnCenter.Size = new System.Drawing.Size(75, 23);
+            this.btnCenter.TabIndex = 4;
+            this.btnCenter.Text = "Középre";
+            this.btnCenter.UseVisualStyleBackColor = true;
+            this.btnCenter.Click += new System.EventHandler(this.btnCenter_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(444, 276);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(75, 23);
+            this.btnRight.TabIndex = 5;
+            this.btnRight.Text = "Jobbra";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnMaxRight
+            // 
+            this.btnMaxRight.Location = new System.Drawing.Point(540, 276);
+            this.btnMaxRight.Name = "btnMaxRight";
+            this.btnMaxRight.Size = new System.Drawing.Size(75, 23);
+            this.btnMaxRight.TabIndex = 6;
+            this.btnMaxRight.Text = "Jobb szélre";
+            this.btnMaxRight.UseVisualStyleBackColor = true;
+            this.btnMaxRight.Click += new System.EventHandler(this.btnMaxRight_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(237, 276);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnLeft.TabIndex = 7;
+            this.btnLeft.Text = "Balra";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnMaxLeft
+            // 
+            this.btnMaxLeft.Location = new System.Drawing.Point(131, 276);
+            this.btnMaxLeft.Name = "btnMaxLeft";
+            this.btnMaxLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnMaxLeft.TabIndex = 8;
+            this.btnMaxLeft.Text = "Bal szélre";
+            this.btnMaxLeft.UseVisualStyleBackColor = true;
+            this.btnMaxLeft.Click += new System.EventHandler(this.btnMaxLeft_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(337, 323);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(75, 23);
+            this.btnDown.TabIndex = 9;
+            this.btnDown.Text = "Le";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnBot
+            // 
+            this.btnBot.Location = new System.Drawing.Point(337, 370);
+            this.btnBot.Name = "btnBot";
+            this.btnBot.Size = new System.Drawing.Size(75, 23);
+            this.btnBot.TabIndex = 10;
+            this.btnBot.Text = "Aljára";
+            this.btnBot.UseVisualStyleBackColor = true;
+            this.btnBot.Click += new System.EventHandler(this.btnBot_Click);
+            // 
+            // btnAtlathatoNovel
+            // 
+            this.btnAtlathatoNovel.Location = new System.Drawing.Point(22, 44);
+            this.btnAtlathatoNovel.Name = "btnAtlathatoNovel";
+            this.btnAtlathatoNovel.Size = new System.Drawing.Size(75, 23);
+            this.btnAtlathatoNovel.TabIndex = 11;
+            this.btnAtlathatoNovel.Text = "Növel";
+            this.btnAtlathatoNovel.UseVisualStyleBackColor = true;
+            this.btnAtlathatoNovel.Click += new System.EventHandler(this.btnAtlathatoNovel_Click);
+            // 
+            // btnAtlathatoCsokkent
+            // 
+            this.btnAtlathatoCsokkent.Location = new System.Drawing.Point(125, 44);
+            this.btnAtlathatoCsokkent.Name = "btnAtlathatoCsokkent";
+            this.btnAtlathatoCsokkent.Size = new System.Drawing.Size(75, 23);
+            this.btnAtlathatoCsokkent.TabIndex = 12;
+            this.btnAtlathatoCsokkent.Text = "Csökkent";
+            this.btnAtlathatoCsokkent.UseVisualStyleBackColor = true;
+            this.btnAtlathatoCsokkent.Click += new System.EventHandler(this.btnAtlathatoCsokkent_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox1.Controls.Add(this.btnMeretNovel);
+            this.groupBox1.Controls.Add(this.btnMeretCsokkent);
+            this.groupBox1.Location = new System.Drawing.Point(255, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(241, 121);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Méret";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox2.Controls.Add(this.btnAtlathatoNovel);
+            this.groupBox2.Controls.Add(this.btnAtlathatoCsokkent);
+            this.groupBox2.Location = new System.Drawing.Point(255, 430);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(241, 127);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Átláthatóság";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(787, 588);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Meretezos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnAtlathatoNovel;
+        private System.Windows.Forms.Button btnAtlathatoCsokkent;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnMeretNovel;
+        private System.Windows.Forms.Button btnMeretCsokkent;
+        private System.Windows.Forms.Button btnBot;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnMaxLeft;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnMaxRight;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnCenter;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnTop;
+    }
+}
+
---
+++ Mozgatós/mozgatos/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mozgatos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private readonly int meretezo = 5;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMeretNovel_Click(object sender, EventArgs e)
+        {
+            Height = Width += 20;
+        }
+
+        private void btnMeretCsokkent_Click(object sender, EventArgs e)
+        {
+            Height = Width -= 20;
+        }
+
+        private void btnAtlathatoNovel_Click(object sender, EventArgs e)
+        {
+            Opacity += 0.1;
+        }
+
+        private void btnAtlathatoCsokkent_Click(object sender, EventArgs e)
+        {
+            Opacity -= 0.1;
+        }
+
+        private void btnCenter_Click(object sender, EventArgs e)
+        {
+            CenterToScreen();
+        }
+
+        private void btnTop_Click(object sender, EventArgs e)
+        {
+            Top = 0;
+            btnUp.Visible = false;
+            btnBot.Visible = true;
+        }
+
+        private void btnMaxLeft_Click(object sender, EventArgs e)
+        {
+            Left = 0;
+        }
+
+        private void btnBot_Click(object sender, EventArgs e)
+        {
+            Top = Screen.PrimaryScreen.Bounds.Height - Height;
+            btnUp.Visible = true;
+            btnBot.Visible = false;
+        }
+
+        private void btnMaxRight_Click(object sender, EventArgs e)
+        {
+            Left = Screen.PrimaryScreen.Bounds.Width - Width;
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            if (Top - meretezo > 0)
+            {
+                Top -= meretezo;
+            }
+            
+        }
+
+        private void btnDown_Click(object sender, EventArgs e) 
+        {
+            if (Top + meretezo < Height) //ez túl megy valami miatt de csak ez
+            {
+                Top += meretezo;
+            }
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            if (Left - meretezo > 0)
+            {
+                Left -= meretezo;
+            }
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            if (Left + meretezo < Width)
+            {
+                Left += meretezo;
+            }
+        }
+
+        // fel le balra jobbra aztán ne lehessen túl scrollozni
+
+
+
+
+    }
+}
